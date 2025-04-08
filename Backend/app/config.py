65f4c986b3f7f280f7edc7388121dff371d40630
Mateusz <mati.mg01@wp.pl>
2025-04-08 21:03:29 +0200

    Backend Init

---
+++ Backend/app/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+from datetime import timedelta
+
+load_dotenv()
+
+class Config:
+    SUPABASE_URL = os.getenv("SUPABASE_URL")
+    SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=7)
